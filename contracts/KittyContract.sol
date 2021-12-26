755e45fb27bf626c7b6c3e79ce91c22f6d035f11
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2021-12-26 14:41:49 -0500

    implemented getkitty and createkitty methods

--- contracts/KittyContract.sol
+++ contracts/KittyContract.sol
@@ -1,8 +1,10 @@
 pragma solidity ^0.5.12;
 
 import "./IERC721.sol";
+import "./Ownable.sol";
 
-contract KittyContract is IERC721{
+contract KittyContract is Ownable, IERC721{
+    uint256 public constant CREATION_LIMIT_GEN0 =10;
     string public constant name = "ZeeKitties";
     string public constant symbol = "ZK";
 
@@ -27,6 +29,7 @@ contract KittyContract is IERC721{
     mapping (uint256 => address) public kittyIndexToOwner;
     mapping (address => uint256) ownershipTokenCount;
 
+    uint256 public gen0Counter;
     
 
     function balanceOf(address owner) external view returns (uint256 balance){
@@ -68,4 +71,48 @@ contract KittyContract is IERC721{
       return kittyIndexToOwner[_tokenId] == _claimant;
     }
 
+    function getKitty(uint256 _kittyId) public returns(
+        uint256 genes,
+        uint256 birthTime,
+        uint256 mumId,
+        uint256 dadId,
+        uint256 generation
+    ){
+        Kitty storage kitty =  kitties[_kittyId];
+
+        genes = kitty.genes;
+        birthTime = kitty.birthTime;
+        mumId = kitty.mumId;
+        dadId = kitty.dadId;
+        generation = kitty.generation;
+    }
+
+    function createKittyGen0(uint256 _genes) public onlyOwner returns(uint256){
+        require(gen0Counter < CREATION_LIMIT_GEN0);
+        gen0Counter++; 
+        return _createKitty( 0, 0, 0, _genes, msg.sender);
+
+    }
+
+    function _createKitty(
+        uint256 _mumId,
+        uint256 _dadId,
+        uint256 _generation,
+        uint256 _genes,
+        address _owner
+    ) private returns(uint256) {
+        Kitty memory kitty = Kitty({
+            genes: _genes,
+            birthTime: uint64(now),
+            mumId:  uint32(_mumId),
+            dadId:  uint32(_dadId),
+            generation: uint16(_generation)
+        });
+
+        uint256 newKittyId = kitties.push(kitty) -1;
+        emit Birth(_owner, newKittyId, _mumId, _dadId, _genes);
+        _transfer(address(0), _owner, newKittyId);
+
+        return newKittyId;
+    }
 }
---
+++ contracts/Ownable.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.5.0;
+
+contract Ownable {
+    address public _owner;
+
+    constructor () public {
+        _owner = msg.sender;
+    }
+
+    /**
+    * @dev Throws if called by any account other than the owner.
+    */
+    modifier onlyOwner() {
+        require(isOwner(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+    * @dev Returns true if the caller is the current owner.
+    */
+    function isOwner() public view returns (bool) {
+        return (msg.sender == _owner);
+    }
+    
+}
